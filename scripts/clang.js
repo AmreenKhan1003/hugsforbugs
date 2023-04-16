a9c560feaac3c2fd6f40f1bdc610dafbbf284814
AmreenKhan1003 <amreen10032000@gmail.com>
2023-04-16 20:11:44 +0530

    added models and script

---
+++ scripts/clang.js
@@ -0,0 +1,35 @@
+fetch("../models/products.json")
+.then(function(response){
+   return response.json();
+})
+.then(function(products){
+   let placeholder = document.querySelector("#data-output");
+   let out = "";
+   for(let product of products){
+    console.log(product.id)
+    console.log(product.title)
+    console.log(product.description)
+    console.log(product.img)
+      out += `
+      <div class="flex relative pt-10 pb-10 sm:items-center md:w-2/3 mx-auto">
+      <div class="h-full w-6 absolute inset-0 flex items-center justify-center">
+        <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+      </div>
+      <div class="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-red-500 text-white relative z-10 title-font font-medium text-sm">${product.id}</div>
+      <div class="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
+        <div class="flex-shrink-0 w-24 h-24 bg-red-100 text-red-500 rounded-full inline-flex items-center justify-center">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-12 h-12" viewBox="0 0 24 24">
+            <path d="${product.img}"></path>
+          </svg>
+        </div>
+        <div class="flex-grow sm:pl-6 mt-6 sm:mt-0">
+          <h2 class="font-medium title-font text-gray-900 mb-1 text-xl">${product.title}</h2>
+          <p class="leading-relaxed">${product.description}</p>
+        </div>
+      </div>
+    </div>
+      `;
+   }
+ 
+   placeholder.innerHTML = out;
+});
