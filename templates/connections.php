1749e8855b26d2db569f4681edc116ac08c57d40
SafuMod <safwanmodak30@gmail.com>
2023-04-19 22:28:54 +0530

    Using PlanetScale database with netlify integration

--- templates/connections.php
+++ templates/connections.php
@@ -1,27 +1,39 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "hugsforbugs";
-
 
 $name = $_POST["name"];
 $mobile = $_POST["mobile"];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+$_ENV = parse_ini_file('.env');
+
+$hostname = $_ENV['HOST'];
+$dbName = $_ENV['DATABASE'];
+$username = $_ENV['USERNAME'];
+$password = $_ENV['PASSWORD'];
+$ssl = $_ENV['MYSQL_ATTR_SSL_CA'];
+
+// Initialize MYSQL Object
+$mysqli = mysqli_init();
+
+// Assign SSL certificate for the created MYSQL Object
+$mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
+
+// Establishing connection with the planetscale database
+$mysqli->real_connect($hostname, $username, $password, $dbName);
+
+
+// Check database connection
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $mysqli->connect_error);
 }
 
 $sql = "INSERT INTO enrolldata VALUES ('$name', $mobile)";
 
-if ($conn->query($sql) === TRUE) {
+if ($mysqli->query($sql) === TRUE) {
   header('Location: thankyou.html');
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $mysqli . "<br>" . $mysqli->error;
 }
 
-$conn->close();
+$mysqli->close();
+
 ?>
